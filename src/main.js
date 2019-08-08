9c029e59145cfbc82ce3e9315ae54373b415c2d5
zdaoran <993370091@qq.com>
2019-08-08 21:19:25 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from '@/api'
+import ElementUI from 'element-ui'
+import router from '@/router'
 
+Vue.prototype.$http = axios
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
