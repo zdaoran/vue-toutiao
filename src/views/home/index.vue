0b91ce276097f21f564b5ae67aa3f13842eb2af3
zdaoran <993370091@qq.com>
2019-08-09 16:25:19 +0800

    添加导航栏折叠效果

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-container class="box">
-    <el-aside width="200px">
-      <div class="bglogo"></div>
+    <el-aside :width="isCollapse?'64px':'200px'">
+      <div class="bglogo" :class="{small:isCollapse}"></div>
       <el-menu
         default-active="/"
         class="el-menu-vertical-demo"
@@ -11,58 +11,45 @@
         :router="true"
         style="border:none"
         :collapse-transition="false"
+        :collapse="isCollapse"
       >
         <el-menu-item index="/">
-          <template slot="title">
-            <i class="el-icon-s-home"></i>
-            <span>首页</span>
-          </template>
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
         </el-menu-item>
         <el-menu-item index="/article">
-          <template slot="title">
-            <i class="el-icon-document"></i>
-            <span>内容管理</span>
-          </template>
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
         </el-menu-item>
         <el-menu-item index="/image">
-          <template slot="title">
-            <i class="el-icon-picture"></i>
-            <span>素材管理</span>
-          </template>
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
         </el-menu-item>
         <el-menu-item index="/publish">
-          <template slot="title">
-            <i class="el-icon-s-promotion"></i>
-            <span>发布文章</span>
-          </template>
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
         </el-menu-item>
         <el-menu-item index="/comment">
-          <template slot="title">
-            <i class="el-icon-chat-dot-round"></i>
-            <span>评论管理</span>
-          </template>
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
         </el-menu-item>
         <el-menu-item index="/fans">
-          <template slot="title">
-            <i class="el-icon-present"></i>
-            <span>粉丝管理</span>
-          </template>
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
         </el-menu-item>
         <el-menu-item index="/setting">
-          <template slot="title">
-            <i class="el-icon-setting"></i>
-            <span>个人设置</span>
-          </template>
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
         </el-menu-item>
       </el-menu>
     </el-aside>
     <el-container>
       <el-header>
-        <span class="el-icon-s-fold"></span>
+        <span class="el-icon-s-fold" @click="collapse"></span>
         <span>江苏传智播客科技教育有限公司</span>
         <el-dropdown style="float:right" @command="toggle">
           <span class="el-dropdown-link">
-            <img :src="userInfo.photo" alt="" class="avatar">
+            <img :src="userInfo.photo" alt class="avatar" />
             {{userInfo.name}}
             <i class="el-icon-arrow-down el-icon--right"></i>
           </span>
@@ -84,7 +71,8 @@ import store from '@/store'
 export default {
   data () {
     return {
-      userInfo: {}
+      userInfo: {},
+      isCollapse: false
     }
   },
   created () {
@@ -100,6 +88,9 @@ export default {
     },
     toggle (command) {
       this[command]()
+    },
+    collapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -120,6 +111,10 @@ export default {
       width: 100%;
       background: #002244 url(../../assets/images/logo_admin.png) no-repeat
         center / 130px auto;
+    }
+    .small {
+      background: #002244 url(../../assets/images/logo_admin_01.png) no-repeat
+        center / 36px auto;
     }
   }
   .el-header {
